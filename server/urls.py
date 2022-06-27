97e6bfa61ed74dc5924d93d9c8d10a18cd01d2d3
ayush <ayusha259@gmail.com>
2022-06-28 00:42:49 +0530

    sdasddasdasfsa

--- server/urls.py
+++ server/urls.py
@@ -9,4 +9,4 @@ urlpatterns = [
     # path('', TemplateView.as_view(template_name="index.html")),
     path('api/', include('api.urls'))
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
